dc83e0b020e4e50ce7925738d9ae512226761ebe
Michelle Pérez <55998893+MichellePerezS@users.noreply.github.com>
2024-07-21 07:37:29 -0500

    Update Avatar.jsx

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -35,7 +35,7 @@ export default function Avatar() {
           borderRadius='10px'
           boxSize={{ base: '150px', sm: '230px' }} 
           objectFit='cover'
-          href=' https://drive.google.com/file/d/1LlfZVxGwkTXZeXaK92PrlJ-PGfXiRhNj/view?usp=drive_link'  
+          href='https://drive.google.com/file/d/1LlfZVxGwkTXZeXaK92PrlJ-PGfXiRhNj/view?usp=sharing'  
           alt="Michelle's Profile"
         />
         <Box p={1} align="left" maxW={{ base: '100%', sm: '200px' }}> 
